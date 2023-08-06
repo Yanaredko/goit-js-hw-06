a2dbad67d2bfdc4933295cf7e26e43b0a3e2401f
Yanaredko <136071376+Yanaredko@users.noreply.github.com>
2023-08-07 02:53:46 +0300

    fifth task done

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const nameInput = document.getElementById('name-input');
+
+const nameOutput = document.getElementById('name-output');
+
+nameInput.addEventListener('input', () => {
+    const inputValue = nameInput.value;
+
+    const nameToShow = inputValue ? inputValue : "Anonymous";
+
+    nameOutput.textContent = nameToShow;
+});

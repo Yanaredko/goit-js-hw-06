8c557fc813b88ff5566406e355b6dd7d49eb221d
Yanaredko <136071376+Yanaredko@users.noreply.github.com>
2023-08-07 02:36:00 +0300

    third task done

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,11 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryList = document.querySelector('.gallery');
+
+const galleryHTML = images.map(image => {
+  return `<li><img src="${image.url}" alt="${image.alt}"></li>`;
+}).join('');
+
+galleryList.insertAdjacentHTML("beforeend", galleryHTML);

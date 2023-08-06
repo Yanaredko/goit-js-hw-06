20c94809ab52d7deecd81992d5fd4d414dcb65cd
Yanaredko <136071376+Yanaredko@users.noreply.github.com>
2023-08-07 02:28:37 +0300

    second task done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.getElementById('ingredients');
+
+ingredients.forEach((ingredient) => {
+  const listItem = document.createElement('li');
+
+  listItem.classList.add('item');
+
+  listItem.textContent = ingredient;
+
+  ingredientsList.append(listItem);
+});

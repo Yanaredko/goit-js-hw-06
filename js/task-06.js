47145276da668698225ee7ca3837ccd68eed6f08
Yanaredko <136071376+Yanaredko@users.noreply.github.com>
2023-08-07 17:53:28 +0300

    sixth task redone

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,7 @@ const inputElement = document.getElementById('validation-input');
 inputElement.addEventListener('blur', () => {
     const inputValue = inputElement.value;
 
-    const expectedLength = Number(inputElement.getAttribute('data-length'));
+    const expectedLength = Number(inputElement.dataset.length);
 
     if (inputValue.length === expectedLength) {
         inputElement.classList.remove('invalid');

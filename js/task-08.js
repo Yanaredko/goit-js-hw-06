60dc17163bc2ad451c9d325e50cfc25e20c5cf49
Yanaredko <136071376+Yanaredko@users.noreply.github.com>
2023-08-07 19:55:30 +0300

    eighth task redone

--- js/task-08.js
+++ js/task-08.js
@@ -1,22 +1,19 @@
-const loginForm = document.querySelector('.login-form');
-
-loginForm.addEventListener('submit', (event) => {
+document.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const emailInput = loginForm.elements.email;
+    const loginForm = event.target;
 
-    const passwordInput = loginForm.elements.password;
+    const { email, password } = loginForm.elements;
 
-    if (emailInput.value === '' || passwordInput.value === '') {
+    if (email.value === '' || password.value === '') {
         alert('Всі поля повинні бути заповнені');
     } else {
         const formData = {
-            email: emailInput.value,
-            password: passwordInput.value,
+            email: email.value,
+            password: password.value,
         };
 
         console.log(formData);
         loginForm.reset();
     }
-
-})
+});
